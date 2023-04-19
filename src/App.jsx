bac568fd8de686838365a13924f7a4fa51106b91
hazem-alsaqaan <hazem.alsaqaan@gmail.com>
2023-04-19 21:50:13 +0200

    initial confirm code page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import {Routes, Route} from "react-router-dom"
 import Login from "./pages/login/Login"
 import Register from "./pages/register/Register"
 import Forgetpassword from "./pages/forget.password/ForgetPassword"
+import ConfirmCode from "./pages/confirm.code/ConfirmCode"
 function App() {
   return (
     <div className="App">
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Login/>}/>
         <Route path="/register" element={<Register/>}/>
         <Route path="/forgetPassword" element={<Forgetpassword/>}/>
+        <Route path="/confirmCode" element={<ConfirmCode/>}/>
       </Routes>
     </div>
   )
---
+++ src/pages/confirm.code/ConfirmCode.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import "./ConfirmCode.css"
+
+const ConfirmCode =()=>{
+    const myMail ="hazem@gmail.com"
+    return(
+        <>
+            <section className="confirm-code">
+                <div className="container">
+                    <h1>نسيت كلمة المرور</h1>
+                    <h2>{`تم إرسال الكود على البريد الالكتروني ${myMail}`}</h2>
+                    <section className="auth-form">
+                        <form>
+                            <label>الرمز التأكيدي</label>
+                            <div className="code-input">
+                                <input
+                                type="tel"
+                                maxLength={1}
+                                required
+                                />
+                                <input
+                                type="tel"
+                                maxLength={1}
+                                required
+                                />
+                                <input
+                                type="tel"
+                                maxLength={1}
+                                required
+                                />
+                                <input
+                                type="tel"
+                                maxLength={1}
+                                required
+                                />
+                            </div>
+                            {/* time */}
+                            <button>تحقق</button>
+                        </form>
+                    </section>                    
+                </div>
+            </section>
+        </>
+    )
+}
+export default ConfirmCode
