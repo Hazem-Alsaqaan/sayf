416cad7764bd5421be0be80c28df63430a740e4c
hazem-alsaqaan <hazem.alsaqaan@gmail.com>
2023-06-05 01:55:33 +0300

    config global rating about an unit

--- src/components/Ratings/Ratings.jsx
+++ src/components/Ratings/Ratings.jsx
@@ -4,25 +4,25 @@ import { useSelector } from "react-redux";
 
 const Ratings = ()=>{
     const {oneUnit} = useSelector((state)=>state.unitsSlice)
-
+    
     return(
         <>
             <section className="ratings">
                 <h2>التقييمات</h2>
                 <div className="ratings-data">
-                    <h5>{oneUnit?.rating ? oneUnit?.rating.toFixed(1) : 0}</h5>
+                    <h5>{oneUnit?.rating ? oneUnit?.ratingClean.toFixed(1) : 0}</h5>
                     <p>نظافة المكان</p>
                 </div>
                 <div className="ratings-data">
-                    <h5>{oneUnit?.rating ? oneUnit?.rating.toFixed(1) : 0}</h5>
+                    <h5>{oneUnit?.rating ? oneUnit?.ratingService.toFixed(1) : 0}</h5>
                     <p>الخدمة والاستقبال</p>
                 </div>
                 <div className="ratings-data">
-                    <h5>{oneUnit?.rating ? oneUnit?.rating.toFixed(1) : 0}</h5>
+                    <h5>{oneUnit?.rating ? oneUnit?.ratingInfo.toFixed(1) : 0}</h5>
                     <p>مطابقه المعلومات المعروضه عن الشقة</p>
                 </div>
                 <div className="ratings-data">
-                    <h5>{oneUnit?.rating ? oneUnit?.rating.toFixed(1) : 0}</h5>
+                    <h5>{oneUnit?.rating ? oneUnit?.ratingCondition.toFixed(1) : 0}</h5>
                     <p>حالة المكان</p>
                 </div>
             </section>
